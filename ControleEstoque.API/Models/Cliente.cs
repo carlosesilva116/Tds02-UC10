39316cfd1af26bf89ebdb0a51e3a028d93dfba0a
carlosesilva116 <carlos.esilva116@gmail.com>
2026-04-06 22:18:00 -0300

    Primeiro Commit

---
+++ ControleEstoque.API/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
+{
+    public class Cliente : Usuario
+    {
+        [StringLength(14)]
+        public string CPF { get; set; }
+
+        //Um cliente pode fazer Vários pedidos
+        public ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+
+    }
+}
---
+++ ControleEstoque.API/Models/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
+{
+    public class Pedido
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public DateTime DataPedido { get; set; } = DateTime.Now;
+
+        [Required, StringLength(20)]
+        public string Status { get; set; }
+
+        public ICollection<ItemPedido> Itens { get; set; } = new List<ItemPedido>();
+    }
+}
